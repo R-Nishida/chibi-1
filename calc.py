9cf37080b6fa1ceb873fc67202b0da2bca79cc1c
R-Nishida <rnishida@fc.jwu.ac.jp>
2019-10-03 14:17:01 +0900

    calc(a+b)

--- calc.py
+++ calc.py
@@ -1,5 +1,9 @@
 def calc(s):
     return int(s)
 
-ss = input("please input number:")
-print(calc(ss))
+def ope_plus(ss):
+    k = ss.find('+')
+    return calc(ss[:k]) + calc(ss[k+1:])
+    
+ss = input("please input :")
+print('-->', str(ope_plus(ss)))
